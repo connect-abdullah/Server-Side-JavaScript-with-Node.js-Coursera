7d13cd0d1d347f94a99e84c8268386c534b9b395
M Abdullah <150430204+connect-abdullah@users.noreply.github.com>
2025-02-01 08:15:09 +0500

    new assignment

---
+++ Calculator App Testing Practice/Calculator App Part 1/src/calculatorApp.js
@@ -0,0 +1,10 @@
+const addition = (num1, num2) => {
+  // Write the code here
+  return num1 + num2;
+};
+
+const subtraction = (num1, num2) => {
+  return num1 - num2;
+};
+
+module.exports = { addition, subtraction };
